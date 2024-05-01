9bc6bf90ff235fb2f8f28c4fbd1c8ed56016b919
zhaohongran <2500789925@qq.com>
2024-05-01 11:06:34 +0800

    feat:add pic and constants

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState<string | undefined>(undefined);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor:"yellow"}}>
+      <View style={styles.container}>
+        {
+          name ? <Text>My Name is {name}</Text> :
+            <View>
+              <Text>Pls input your name: </Text>
+              <TextInput
+                value={name}
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => setName(text)}>
+              </TextInput>
+            </View>
+        }
+      </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ README.md
@@ -0,0 +1,30 @@
+### One day
+1. 下载后面所需的assets资源 
+2. 分析页面结构
+   - 首页带四个tab: Feed Category Cart Profile
+   - Feed子页面：搜索框，消息提醒
+   - Category子页面： 子子页面
+   - Cart子页面：购物车、订单
+   - Profile 子页面： 我的订单、我的收藏、我的评价、地址管理、最近访问、账户信息、退出
+
+   - 常见页面：
+     - loading page
+     - error page
+     - notification not login page
+     - login page
+     - logout page
+3. 创建所需要的constants(in utils)
+
+### Two day
+1. 安装与导航相关的package: npm install --save react-native-navigation
+
+### Three day
+1. 
+
+### 参考文档
+1. https://reactnative.cn/docs/getting-started React-native库
+2. https://github.com/hongran997/c-shopping-rn 参考项目
+3. https://reactnavigation.org/  React-native 官方推荐的导航库1
+4. https://reactnavigation.org/docs/getting-started/ react-native 官方推荐的导航库
+5. https://wix.github.io/react-native-navigation/docs/before-you-start  官方推荐的导航库2
+6. https://github.com/jquense/yup?tab=readme-ov-file 表单数据校验
