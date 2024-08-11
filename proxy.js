8bbc90f29ce27d849b44fa014ed86aeae3fbfa5d
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2024-08-11 08:52:15 +0800

    feat: proxy

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:8081', // 允许来自localhost:8081的请求
+  // origin: 'http://localhost:8081', // 允许来自localhost:8081的请求
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -15,7 +15,7 @@ app.use(morgan('combined'));
 app.use(
   '/api',
   createProxyMiddleware({
-    target: 'http://shop.huanghanlian.com/api',
+    target: 'https://shop.huanghanlian.com/api',
     changeOrigin: true,
     onProxyRes(proxyRes, req, res) {
       console.log(req, res);
@@ -33,4 +33,3 @@ app.get('/get/user', (req, res) => {
 app.listen(3000, () => {
   console.log('Proxy server is running on port 3000');
 });
-
